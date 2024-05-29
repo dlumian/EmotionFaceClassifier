59500c1abdc1fc92f1c0573e28c04d58c4c0c567
Daniel Lumian <dlumian@gmail.com>
2024-05-29 12:08:45 -0600

    Located additional data source, began notebook and code for analysis

--- README.md
+++ README.md
@@ -1,7 +1,13 @@
 # EmotionFaceClassifier
 
-Project to determine emotion of a face.
+Computer vision project to determine emotion of a face.
 
-Data comes from a [Kaggle competition](https://www.kaggle.com/competitions/challenges-in-representation-learning-facial-expression-recognition-challenge/data).
+This repo is a revisitation of my [emotion_face_classification](https://github.com/dlumian/emotion_face_classification) repo from 2018.
 
-This repo is a revisitation of my [emotion_face_classification](https://github.com/dlumian/emotion_face_classification) repo from 2019.
+## Data Sources
+
+- [Kaggle FER2013](https://www.kaggle.com/competitions/challenges-in-representation-learning-facial-expression-recognition-challenge/data).
+- [Kaggle Facial Recognition Dataset](https://www.kaggle.com/datasets/apollo2506/facial-recognition-dataset/data)
+
+
+
---
+++ src/main.py
@@ -0,0 +1,41 @@
+import os
+import pandas as pd
+import numpy as np
+
+def check_directory_name(target_name) -> bool:
+    """
+    Check if the current directory name matches the target_name.
+    If not, move up a directory and repeat the check.
+    
+    Args:
+        target_name (str): The directory name to match.
+        
+    Returns:
+        bool: True if the current directory name matches the target_name, False otherwise.
+    """
+    # Get the current directory path
+    current_dir = os.getcwd()
+    
+    # Extract the directory name from the path
+    current_dir_name = os.path.basename(current_dir)
+    
+    # Check if the current directory name matches the target_name
+    if current_dir_name == target_name:
+        print(f'Directory set to {current_dir}, matches target dir sting {target_name}.')
+        return True
+    else:
+        # Move up a directory
+        os.chdir('..')
+        # Check if we have reached the root directory
+        if os.getcwd() == current_dir:
+            return False
+        # Recursively call the function to check the parent directory
+        return check_directory_name(target_name)
+
+class EmoFaceClassifier():
+    '''
+    Class for ingesting, analyzing, and predicting emotional facial expressions
+    '''
+
+    def __init__(self) -> None:
+        pass
