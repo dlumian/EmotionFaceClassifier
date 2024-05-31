0095ca36f941fe486f5c721db28a00d61553dd86
Daniel Lumian <dlumian@gmail.com>
2024-05-30 18:10:54 -0600

    Added functionality to plot example images from each emotion category

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ import os
 import pandas as pd
 import numpy as np
 import plotly.express as px
+import matplotlib.pyplot as plt
 
 def check_directory_name(target_name) -> bool:
     """
@@ -86,6 +87,42 @@ def add_bar_totals(fig, df, col, y_offset=1000):
             ),
         )
     return fig
+
+def show_example_images(df, group_col='emotion', image_col='image', save_path=None, samples=1):  
+    # fig=plt.figure(figsize=(10, 3))
+    n_cols = df[group_col].nunique()
+    n_rows = samples
+
+    fig_width = 10
+    fig_height = samples * 2
+    
+    # fig, axes = plt.subplots(nrows=n_rows, ncols=n_cols, squeeze=False, figsize=(num_images_per_row * 5, num_rows * 5))
+    fig, axes = plt.subplots(nrows=n_rows, ncols=n_cols, squeeze=False, figsize=(fig_width, fig_height))
+
+    for i in range(n_rows):
+        samples_df = df.groupby(group_col).sample(n=1).reset_index()
+        samples_df.sort_values(by=group_col, inplace=True)
+        for idx, row in samples_df.iterrows():
+            ax = axes[i, idx]
+            ax.imshow(np.array(row[image_col]), cmap='gray')
+            ax.axis('off')
+
+    # Add titles to each column
+    emo_labels = samples_df[group_col].to_list()
+    for col_idx, label in enumerate(emo_labels):
+        axes[0, col_idx].set_title(f"{label}")
+
+    plt.tight_layout()    
+    if save_path:
+        plt.savefig(save_path)
+    plt.show()
+    plt.close()
+
+def convert_pixels_to_array(pixels):
+    'Reshape pixel arrays into correct format'
+    array = np.array([int(x) for x in pixels.split(' ')]).reshape(48,48)
+    array = np.array(array, dtype='uint8')
+    return array
 
 class EmoFaceClassifier():
     '''
