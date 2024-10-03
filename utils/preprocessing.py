f1515cce1ac8721e2e3c345d1c689c041071f201
Daniel Lumian <dlumian@gmail.com>
2024-10-03 16:04:12 -0600

    Separated preprocessing from EDA for more rapid iteration on data pipeline

---
+++ utils/preprocessing.py
@@ -0,0 +1,25 @@
+import os
+import numpy as np
+import pandas as pd
+from PIL import Image
+
+
+def convert_pixels_to_array(pixels):
+    'Reshape pixel arrays into 2-D format from flat vector'
+    array = np.array([int(x) for x in pixels.split(' ')]).reshape(48, 48)
+    array = np.array(array, dtype='uint8')
+    return array
+
+def save_image(row):
+    # Set path, verify dirs exist
+    base_path = 'data'
+    emot = row['emotion']
+    dir_path = os.path.join(base_path, row['usage'], emot)
+    os.makedirs(dir_path, exist_ok=True)
+    # Final output name
+    f_name = f"{emot}-{row['emo_count_id']}.jpg"
+    final_path = os.path.join(dir_path, f_name)
+    # Convert and save the array to grayscale
+    img = Image.fromarray(row['image'].astype('uint8'), 'L')
+    img.save(final_path)
+    return final_path
